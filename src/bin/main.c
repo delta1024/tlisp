a0b985ec89582ff4c2834c47cbcbaca86c1867e5
Jake Stannix <jakestannix@gmail.com>
2024-06-04 19:55:30 -0600

    feat(binary): implemented repl

--- src/bin/main.c
+++ src/bin/main.c
@@ -1,21 +1,19 @@
 #include "tlisp/state.h"
 #include <stdio.h>
+#include <string.h>
 #include <tlisp.h>
 
 int main(void) {
     tlisp_state *state = tlisp_state_open();
-    tlisp_error error;
-
-    if (!tlisp_state_loadbuffer(state, NULL, 0))
-        goto had_err;
-
-    if (!tlisp_state_call(state, 0, 0, 0))
-        goto had_err;
-    goto cleanup;
-had_err:
-    tlisp_error_next(state, &error);
-    fprintf(stderr, "%.*s\n", error.mlen, error.message);
-    tlisp_error_free(&error);
-cleanup:
+    char buffer[1024];
+    for (;;) {
+        printf("> ");
+        if(!fgets(buffer, 1024, stdin)) {
+            printf("\n");
+            break;
+        }
+        tlisp_state_loadbuffer(state, buffer, strlen(buffer));
+        tlisp_state_call(state, 0,0,0);
+    }
     tlisp_state_close(state);
 }
