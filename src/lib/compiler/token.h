ca8d170e64cb8ed79ca0d85683e96ce1a00cd071
Jake Stannix <jakestannix@gmail.com>
2024-06-05 16:29:08 -0600

    feat(tokens): language tokens defined

--- src/lib/compiler/token.h
+++ src/lib/compiler/token.h
@@ -2,8 +2,34 @@
 #define _TLISP_TOKEN_H__
 
 typedef enum {
-    TOKEN_ERROR,
-    TOKEN_EOF,
+    TOKEN_ERROR = -1,
+    TOKEN_EOF = 0,
+    TOKEN_PLUS = 1,
+    TOKEN_MINUS,
+    TOKEN_STAR,
+    TOKEN_SLASH,
+    TOKEN_TICK,
+    TOKEN_LPAREN,
+    TOKEN_RPAREN,
+
+    TOKEN_EQUAL,
+    TOKEN_EQUAL_EQUAL,
+    TOKEN_GT,
+    TOKEN_GT_EQUAL,
+    TOKEN_LT,
+    TOKEN_LT_EQUAL,
+    TOKEN_BANG,
+    TOKEN_BANG_EQUAL,
+
+    TOKEN_STRING,
+    TOKEN_NUMBER,
+
+    TOKEN_NAME,
+    TOKEN_QUOTE,
+    TOKEN_CONS,
+    TOKEN_CAR,
+    TOKEN_TRUE,
+    TOKEN_FALSE,
 } ttoken_t;
 
 typedef struct {
