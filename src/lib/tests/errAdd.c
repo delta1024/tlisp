62000c12bc63886b106e3d34e86b068db310dde1
Jake Stannix <jakestannix@gmail.com>
2024-06-04 18:06:56 -0600

    refacor(tests): test now use unity framework

--- src/lib/tests/errAdd.c
+++ src/lib/tests/errAdd.c
@@ -1,48 +1,39 @@
-#include "test_common.h"
 #include "tlisp/errors.h"
 #include "tlisp/state.h"
 #include "tlisp/types.h"
+#include "unity.h"
+#include "unity_internals.h"
 #include <string.h>
 #include <tlisp.h>
 static tlisp_state *state;
 static const char *message = "unterminated string";
 static tlisp_error error;
+
+void setUp() {
+    state = tlisp_state_open();
+    error.message = NULL;
+}
+void tearDown(){
+    if(error.message != NULL) {
+        tlisp_error_free(&error);
+    }
+    tlisp_state_close(state);
+}
 #ifdef CMAKE_BUILD_TESTS
 int errAdd(void) {
 #else
-int main() {
+void errAdd(void) {
 #endif /* ifdef CMAKE_BUILD_TESTS */
-
-    state = tlisp_state_open();
     tlisp_error_report(state, TLISP_ERR_UNTERMITATED_STRING, message);
-
-    if (!tlisp_error_next(state, &error)) {
-        test_fail("Could not get error from state");
-        goto defer;
-    }
-
-    if (error.code != TLISP_ERR_UNTERMITATED_STRING) {
-        test_fail("Wrong error code; expected %d, got %d",
-                  TLISP_ERR_UNTERMITATED_STRING, error.code);
-        goto defer_err;
-    }
-
+    TEST_ASSERT_MESSAGE(tlisp_error_next(state, &error),"Could not get error from state" );
+    TEST_ASSERT_EQUAL_INT_MESSAGE(TLISP_ERR_UNTERMITATED_STRING, error.code, "wrong error code");
     int err_len = strlen(message);
-    if (error.mlen != err_len) {
-        test_fail("Wrong error len; expected %d, got %d", err_len, error.mlen);
-        goto defer_err;
-    }
+    TEST_ASSERT_EQUAL_INT_MESSAGE(err_len, error.mlen, "Wrong error code");
+    TEST_ASSERT_EQUAL_STRING_LEN_MESSAGE(message, error.message, error.mlen, "Wrong error message");
+}
 
-    if (memcmp(message, error.message, error.mlen) != 0) {
-        test_fail("Wrong error message; expected %s, get %.*s", message,
-                  error.mlen, error.message);
-        goto defer_err;
-    }
-    test_pass();
-
-defer_err:
-    tlisp_error_free(&error);
-defer:
-    tlisp_state_close(state);
-    return 0;
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(errAdd);
+    UNITY_END();
 }
--- src/lib/tests/test_common.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TLISP_TEST_COMMON_H__
-#define _TLISP_TEST_COMMON_H__
-
-int _test_fail(const char *callee, const char *message, ...);
-int _test_pass(const char *callee);
-#define test_pass() _test_pass(__func__)
-#define test_fail(...) _test_fail(__func__, __VA_ARGS__)
-
-#endif // !_TLISP_TEST_COMMON_H__
